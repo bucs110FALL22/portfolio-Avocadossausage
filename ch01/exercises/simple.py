90096fa32c7f15def70f42b7e3813132e3a37116
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-09-06 21:50:28 +0000

    exercise

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -8,7 +8,9 @@ print(10 % 15)
 print(10 % 10)
 print(0 % 10)
 print(10 / 15)
-## 
+
+## the number is technically infinite, but the program can't infinitely print out the number
+
 rate = float(input("Please input the current exchange rate from Euro to Dollar"))
 amount = float(input("Please input the amount of Euros you have to exchange"))
 total = (amount / rate)
