1c18ae5b9e17094b264b18e6e7156b8ca632f6ee
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-09-08 16:53:06 +0000

    Lab Ch02

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,23 @@
-
 #Part A
+import random
+
 weeks = 16
+print(weeks , type(weeks))
 classes = 5
+print(classes, type(classes))
 tuition = 6000
+print(tuition , type(tuition))
+classes_per_week = 2
+print(classes_per_week, type(classes_per_week))
 cost_per_week = ((tuition / classes) / weeks)
+print(cost_per_week, type(cost_per_week))
+cost_per_class = (cost_per_week / classes_per_week)
+print(cost_per_class, type(cost_per_class))
 print("Cost per week:", cost_per_week)
+print("This is the cost per class :) ", cost_per_class)
 
 
 #Part B
+list = ["ketchup", 20, 50.5, 1, "burger"]
+random_obj = random.choice(list)
+print(random_obj)
