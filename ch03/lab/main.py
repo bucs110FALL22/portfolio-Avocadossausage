b71e369bb55ff4708d7e73cbf7b8cc8740cf4233
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-09-23 21:56:19 +0000

    lab ch3

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,7 +1,10 @@
 import turtle #1. import modules
 import random
+import pygame
+import math
 
 #Part A
+
 window = turtle.Screen() # 2.  Create a screen
 window.bgcolor('lightblue')
 
@@ -16,11 +19,88 @@ michelangelo.up() # 4. Pick up the pen so we don’t get lines
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
+michelangelo.speed(1)
+leonardo.speed(1)
 
 ## 5. Your PART A code goes here
+randnum = random.randrange(1, 101)
+michelangelo.forward(randnum)
+leonardo.forward(randnum)
 
+michelangelo.setposition(-100, 20)
+leonardo.setposition(-100, -20)
+
+for looper in [1, 11]:
+  randnum = random.randrange(0, 11)
+  michelangelo.forward(randnum)
+  leonardo.forward(randnum)
+
+window.exitonclick()
 
 # PART B - complete part B here
 
+pygame.init()
+window = pygame.display.set_mode((500, 500))
+
+def shapeDrawer(num_sides):
+
+  coords = []
+  num_sides = num_sides
+  side_length = 25
+  offset = 50
+
+  for i in range(num_sides):
+    theta = (2.0 * math.pi * n) / num_sides
+    x = side_length * math.cos(theta) + offset
+    y = side_length * math.sin(theta) + offset
+    coords.append([x,y])
+
+  return coords
+
+
+window.fill("pink")
+pygame.display.flip()
+
+pygame.draw.polygon(window, "red", shapeDrawer(3))
+pygame.display.flip()
+pygame.time.delay(500)
+
+window.fill("pink")
+pygame.display.flip()
+
+pygame.draw.polygon(window, "green", shapeDrawer(4))
+pygame.display.flip()
+pygame.time.delay(500)
+
+window.fill("pink")
+pygame.display.flip()
+
+pygame.draw.polygon(window, "blue", shapeDrawer(4))
+pygame.display.flip()
+pygame.time.delay(500)
+
+window.fill("pink")
+pygame.display.flip()
+
+pygame.draw.polygon(window, "purple", shapeDrawer(6))
+pygame.display.flip()
+pygame.time.delay(500)
+
+window.fill("pink")
+pygame.display.flip()
+
+pygame.draw.polygon(window, "yellow", shapeDrawer(9))
+pygame.display.flip()
+pygame.time.delay(500)
+
+window.fill("pink")
+pygame.display.flip()
+
+pygame.draw.polygon(window, "yellow", shapeDrawer(360))
+pygame.display.flip()
+pygame.time.delay(500)
+
+window.fill("pink")
+pygame.display.flip()
 
 window.exitonclick()
