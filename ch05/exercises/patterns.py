36d11722849772c310108bf9c412d744e0b24e48
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-10-11 02:57:24 +0000

    exercise ch 5 attempt

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,9 +1,21 @@
-rowmaker = 1
-def star_pyramid():
-  print("Please enter number of rows")
-  rows = int(input(":"))
-  for i in rows:
-    print( "*" * rowmaker)
-    rowmaker + 1
 
-def rstar_pyramid(): 
+def rstar_pyramid():
+  rows = int(input("Enter a number of rows:  "))
+  for i in range(rows):
+    for n in range(i+1):
+      print( "*" * rows, end = "")
+      print("\n")
+      rows -= 1
+
+rstar_pyramid()
+
+
+def star_pyramid():
+  starcount = 1
+  rows = int(input("Enter a number of rows:  "))
+  for i in range(rows):
+    for n in range(i + 1):
+      print( "*" * starcount, end = "")
+      starcount += 1
+      print("\n")
+star_pyramid()
