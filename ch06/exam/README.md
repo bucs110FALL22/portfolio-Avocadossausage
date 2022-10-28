c28438857c59f8d1773aa21ad293a4bc906daa12
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-10-28 17:46:25 +0000

    exam

---
+++ ch06/exam/README.md
@@ -0,0 +1,9 @@
+# CS110 Midterm Exam
+
+## SHORT DESCRIPTION *(This program draws a spooky ghost using the turtle library)*
+
+## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
+
+## REFERENCES *(any resources you use outside of class materials)*
+
+## MISCELLANEOUS COMMENTS *(Changed idea from bowling pin to a Halloween theme. )*
---
+++ ch06/exam/tester.py
@@ -0,0 +1,27 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("pink")
+window.screensize(1000, 1000)
+spook = turtle.Turtle()
+
+spook.color("white")
+
+def circle(r):
+  spook.pd()
+  spook.begin_fill()
+  spook.circle(r)
+  spook.end_fill()
+  spook.pu()
+  
+def eyes():
+  circle(50)
+  spook.goto(0, 100)
+  circle(50)
+  spook.goto(0, -100)
+
+def main():
+  turtle.exitonclick()
+  eyes()
+main()
+
