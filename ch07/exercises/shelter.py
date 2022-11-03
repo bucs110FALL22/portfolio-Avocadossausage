84f089f8897961d39a8d8b887994b63e7b3dac92
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-11-03 02:56:58 +0000

    animal

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,18 @@
+import time
+
+class Animal:
+  def __init__(self, name, type):
+    self.id = time.strftime("%d/%m/%Y")
+    self.name = name
+    self.type = type
+    self.arrived = time.strftime("%d/%m/%Y")
+    self.adopted = None
+
+def set_adopted(self):
+  self.adopted = time.strftime("%d/%m/%Y")
+
+def main():
+  fido = Animal("fido", "cat")
+  fido.setAdopted()
+  print()
+main()
---
+++ ch07/lab/Rectangle.py
@@ -0,0 +1,13 @@
+
+class Rectangle:
+  
+  def __init__(self, x, y, h, w):
+    self.x = x
+    self.y  = y
+    self.height = h
+    self.width = w
+  
+  def __str__(self):
+    dimensions = str(self.x, self.y, self.height, self.width)
+    return dimensions
+
