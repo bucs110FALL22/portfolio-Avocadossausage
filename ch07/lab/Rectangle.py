5fec107db8ddba7f6d0a91897f01769825847ea3
Sophia Lee <112440155+avocadossausage@users.noreply.github.com>
2022-11-05 02:08:30 +0000

    lab 7

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -2,12 +2,23 @@
 class Rectangle:
   
   def __init__(self, x, y, h, w):
+
     self.x = x
     self.y  = y
-    self.height = h
-    self.width = w
-  
+    self.h = h
+    self.w = w
+
+    if self.x < 0:
+        self.x = 0
+    if self.y < 0:
+        self.y = 0
+    if self.h < 0:
+        self.h = 0
+    if self.w < 0:
+        self.w = 0
+      
   def __str__(self):
-    dimensions = str(self.x, self.y, self.height, self.width)
+    
+    dimensions = str(self.x, self.y, self.h, self.w)
     return dimensions
 
